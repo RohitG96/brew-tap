3694fe0987d9a693c4d8f2f8de28853d65b7b626
Example Example <example@example.org>
2019-02-28 16:21:28 +0530

    version change

--- myfirstbrewscript.rb
+++ myfirstbrewscript.rb
@@ -2,7 +2,7 @@
 #                https://www.rubydoc.info/github/Homebrew/brew/master/Formula
 # PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 class Myfirstbrewscript < Formula
-  version "1.1.1"
+  version "2.1.1"
   desc ""
   homepage ""
   url "https://bitbucket.org/ariya/phantomjs/downloads/phantomjs-2.1.1-linux-x86_64.tar.bz2"
