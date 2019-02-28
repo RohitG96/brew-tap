2ae064268b76d4c6eccbaf7f387aa85413ecc9b8
Example Example <example@example.org>
2019-02-28 16:23:59 +0530

    removes amyfirstbrewscript

--- phantomjs.rb
+++ phantomjs.rb
@@ -1,7 +1,7 @@
 # Documentation: https://docs.brew.sh/Formula-Cookbook
 #                https://www.rubydoc.info/github/Homebrew/brew/master/Formula
 # PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
-class Myfirstbrewscript < Formula
+class Phantomjs < Formula
   version "2.1.1"
   desc ""
   homepage ""
