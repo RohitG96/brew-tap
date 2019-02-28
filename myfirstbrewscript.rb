2b819e7311269314bc5742a8765230ce623ba577
Example Example <example@example.org>
2019-02-28 16:20:58 +0530

    phantomjs

--- myfirstbrewscript.rb
+++ myfirstbrewscript.rb
@@ -2,17 +2,18 @@
 #                https://www.rubydoc.info/github/Homebrew/brew/master/Formula
 # PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 class Myfirstbrewscript < Formula
+  version "1.1.1"
   desc ""
   homepage ""
-  url "https://github.com/RohitG96/myfirstbrewscript/archive/v1.0.0.tar.gz"
-  sha256 "91191c63b3d95c617c4ffd3e3335d2910d9c8d9ba8b2bd4342d7ef5c28a6e759"
+  url "https://bitbucket.org/ariya/phantomjs/downloads/phantomjs-2.1.1-linux-x86_64.tar.bz2"
+  sha256 "86dd9a4bf4aee45f1a84c9f61cf1947c1d6dce9b9e8d2a907105da7852460d2f"
   # depends_on "cmake" => :build
 
   depends_on :arch => :x86_64
   depends_on :linux
   
   def install
-    bin.install "myfirstbrewscript"
+    bin.install "bin/phantomjs"
   end
 
 end
