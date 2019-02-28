ee1818005925cd8ba2bfe0ef6bb9fc416c554b60
Example Example <example@example.org>
2019-02-28 16:30:43 +0530

    adds version parameterized

--- phantomjs.rb
+++ phantomjs.rb
@@ -5,7 +5,7 @@ class Phantomjs < Formula
   version "2.1.1"
   desc ""
   homepage ""
-  url "https://bitbucket.org/ariya/phantomjs/downloads/phantomjs-2.1.1-linux-x86_64.tar.bz2"
+  url "https://bitbucket.org/ariya/phantomjs/downloads/phantomjs-#{version}-linux-x86_64.tar.bz2"
   sha256 "86dd9a4bf4aee45f1a84c9f61cf1947c1d6dce9b9e8d2a907105da7852460d2f"
   # depends_on "cmake" => :build
 
